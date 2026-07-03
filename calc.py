7dd02c9ab2bae6254f65c6f9f4a6b72f7688f059
lvdejesus <lvdejesus1@up.edu.ph>
2026-07-03 12:20:06 +0800

    added subtraction

--- calc.py
+++ calc.py
@@ -2,7 +2,15 @@ def add(x, y):
     answer = x + y
     return print(answer)
 
+def sub(x, y):
+    answer = x - y
+    return print(answer)
+
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-
-add(num1, num2)
+print("Operations: add, sub")
+op = input("Enter operation: ")
+if op == "add":
+    add(num1, num2)
+elif op == "sub":
+    sub(num1, num2)
