110896caad26cbc5da15e9b0bb93bd536a118ee7
Pan <axmamaradlo16@gmail.com>
2026-07-03 12:23:47 +0800

    test 2

--- calc.py
+++ calc.py
@@ -6,11 +6,17 @@ def sub(x, y):
     answer = x - y
     return print(answer)
 
+def mult(x, y):
+    answer = x * y
+    return print(answer)
+
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-print("Operations: add, sub")
+print("Operations: add, sub, mult")
 op = input("Enter operation: ")
 if op == "add":
     add(num1, num2)
 elif op == "sub":
     sub(num1, num2)
+elif op == "mult":
+    mult(num1, num2)
