25db5673eee59964ab06a0d539a263afa4695050
lvdejesus <lvdejesus1@up.edu.ph>
2026-07-03 12:22:29 +0800

    add division

--- calc.py
+++ calc.py
@@ -6,11 +6,17 @@ def sub(x, y):
     answer = x - y
     return print(answer)
 
+def div(x, y):
+    answer = x / y
+    return print(answer)
+
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-print("Operations: add, sub")
+print("Operations: add, sub, div")
 op = input("Enter operation: ")
 if op == "add":
     add(num1, num2)
 elif op == "sub":
     sub(num1, num2)
+elif op == "div":
+    div(num1, num2)
